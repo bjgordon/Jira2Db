7d6c60d395f3ccc72ea4ef8a19c7fb4b6aa4dcfc
Brendan Gordon <brendan.gordon@gmail.com>
2012-10-01 21:10:38 +1000

    Rearranged test classes to suit class package locn

--- Jira2Db/src/main/java/com/gordcorp/jira2db/App.java
+++ Jira2Db/src/main/java/com/gordcorp/jira2db/App.java
@@ -1,33 +1,26 @@
-/*
-
-	Copyright 2012 Brendan Gordon
-  
-	Jira2Db is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*******************************************************************************
+ * Copyright 2012 Brendan Gordon
+ * 	
+ * 	This file is part of Jira2Db.
+ * 	
+ * 	Jira2Db is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ * 	
+ * 	Jira2Db is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ * 	
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with Jira2Db.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
 package com.gordcorp.jira2db;
 
 import java.net.URI;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
-import java.util.Properties;
 
-import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,8 +29,6 @@ import com.atlassian.jira.rest.client.NullProgressMonitor;
 import com.atlassian.jira.rest.client.auth.BasicHttpAuthenticationHandler;
 import com.atlassian.jira.rest.client.domain.Issue;
 import com.atlassian.jira.rest.client.internal.jersey.JerseyJiraRestClientFactory;
-import com.gordcorp.jira2db.persistence.SqlSessionFactorySingleton;
-import com.gordcorp.jira2db.persistence.mapper.DbMapper;
 import com.gordcorp.jira2db.util.PropertiesWrapper;
 
 public class App {
@@ -51,24 +42,10 @@ public class App {
 		testJira();
 		// testJdbc();
 		// testJpa();
-		testMybatis();
+		// testMybatis();
 
 		Jira jira = new Jira();
 
-	}
-
-	private static void testMybatis() {
-		SqlSession session = SqlSessionFactorySingleton.instance()
-				.openSession();
-		try {
-			DbMapper mapper = session.getMapper(DbMapper.class);
-			List<Long> ids = mapper.getIdsFromMap();
-
-			System.out.println("ID:" + ids.get(0));
-
-		} finally {
-			session.close();
-		}
 	}
 
 	public static void testJira() throws Exception {
@@ -83,28 +60,4 @@ public class App {
 		System.out.println(issue);
 	}
 
-	public static void testJdbc() {
-		String url = PropertiesWrapper.get("url");
-		Properties props = new Properties();
-		props.setProperty("user", PropertiesWrapper.get("username"));
-		props.setProperty("password", PropertiesWrapper.get("password"));
-		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection(url, props);
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from map");
-			while (rs.next()) {
-				long id = rs.getLong(1);
-				System.out.println("ID:" + id);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
 }
