ba40b4f55c1ed49767324e2a0dd69980634d783a
Brendan Gordon <brendan.gordon@gmail.com>
2012-10-19 09:31:45 +1100

    Added JiraCustomFieldDto for reading and storing jira custom fields

---
+++ src/main/java/com/gordcorp/jira2db/persistence/dto/JiraCustomFieldDto.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright 2012 Brendan Gordon
+ * 	
+ * 	This file is part of Jira2Db.
+ * 	
+ * 	Jira2Db is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ * 	
+ * 	Jira2Db is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ * 	
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with Jira2Db.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package com.gordcorp.jira2db.persistence.dto;
+
+public class JiraCustomFieldDto extends GenericDto {
+
+	private static final long serialVersionUID = 2898156073530420987L;
+
+	String jiraKey;
+	String id;
+	String name;
+	String type;
+	String value;
+
+	public String getJiraKey() {
+		return jiraKey;
+	}
+
+	public void setJiraKey(String jiraKey) {
+		this.jiraKey = jiraKey;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
