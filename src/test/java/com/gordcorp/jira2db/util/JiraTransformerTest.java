16cf9e229c21190cb3ee9d7c979928331136644c
Brendan Gordon <brendan.gordon@gmail.com>
2012-10-30 00:11:58 +1100

    Adding custom fields coverage

--- src/test/java/com/gordcorp/jira2db/util/JiraTransformerTest.java
+++ src/test/java/com/gordcorp/jira2db/util/JiraTransformerTest.java
@@ -21,8 +21,12 @@ package com.gordcorp.jira2db.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Test;
 
+import com.atlassian.jira.rest.client.domain.Field;
 import com.atlassian.jira.rest.client.domain.Issue;
 import com.gordcorp.jira2db.persistence.dto.JiraIssueDto;
 
@@ -30,18 +34,40 @@ public class JiraTransformerTest {
 
 	@Test
 	public void test_toJiraIssueDto() {
+		Collection<Field> fields = new ArrayList<Field>();
+		fields.add(new Field("id", "name", "type", "value"));
+		fields.add(new Field("id2", "name2", "type", "value"));
+
 		Issue issue = new Issue("test summary", null, "http://example.com",
 				null, null, null, "test-1", null, null, null, null, null, null,
-				null, null, null, null, null, null, null, null, null, null,
+				null, null, null, null, null, fields, null, null, null, null,
 				null, null, null, null, null, null);
 
 		JiraIssueDto dto = JiraTransformer.toJiraIssueDto(issue);
 		assertEquals(issue.getKey(), dto.getJiraKey());
 		assertEquals(issue.getSummary(), dto.getSummary());
 		assertEquals(issue.getTransitionsUri(), dto.getJiraUri());
+		assertEquals(2, dto.getCustomFields().size());
+
+		assertEquals(issue.getField("id").getId(), dto.getCustomFields().get(0)
+				.getId());
+		assertEquals(issue.getField("id").getName(),
+				dto.getCustomFields().get(0).getName());
+		assertEquals(issue.getField("id").getType(),
+				dto.getCustomFields().get(0).getType());
+		assertEquals(issue.getField("id").getValue(), dto.getCustomFields()
+				.get(0).getValue());
+
+		assertEquals(issue.getField("id2").getId(), dto.getCustomFields()
+				.get(1).getId());
+		assertEquals(issue.getField("id2").getName(), dto.getCustomFields()
+				.get(1).getName());
+		assertEquals(issue.getField("id2").getType(), dto.getCustomFields()
+				.get(1).getType());
+		assertEquals(issue.getField("id2").getValue(), dto.getCustomFields()
+				.get(1).getValue());
 
 		// todo check other fields
 
 	}
-
 }
